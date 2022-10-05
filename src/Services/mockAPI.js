28a48c0875a9589a7fb1e3502894bdd39be39e70
santi leon <leonskennedy146@gmail.com>
2022-10-05 11:47:45 -0300

    revisar desafio de primera entrega del proyecto

--- src/Services/mockAPI.js
+++ src/Services/mockAPI.js
@@ -1,51 +1,65 @@
 const items=[
     {
-    "id": 1,
-    "title": "Namfix",
-    "category": "Tracker",
-    "price": 68
-  }, {
-    "id": 2,
-    "title": "Cardguard",
-    "category": "Land Cruiser",
-    "price": 33
-  }, {
-    "id": 3,
-    "title": "Temp",
-    "category": "XC70",
-    "price": 80
-  }, {
-    "id": 4,
+    "id": "1",
     "title": "Naruto",
     "detail": "Manga de 50 paginas",
     "price": 2000,
-    "img": "/assets/naruto.jpg"
+    "img": "/assets/public/naruto.jpg"
   }, {
-    "id": 5,
-    "title": "Fintone",
-    "category": "tC",
-    "price": 5
+    "id": "2",
+    "title": "Naruto",
+    "detail": "Manga de 50 paginas",
+    "price": 2000,
+    "img": "/assets/public/naruto.jpg"
+  }, {
+    "id": "3",
+    "title": "Naruto",
+    "detail": "Manga de 50 paginas",
+    "price": 2000,
+    "img": "/assets/public/naruto.jpg"
+  }, {
+    "id": "4",
+    "title": "Naruto",
+    "detail": "Manga de 50 paginas",
+    "price": 2000,
+    "img": "/assets/public/naruto.jpg"
+  }, {
+    "id": "5",
+    "title": "Naruto",
+    "detail": "Manga de 50 paginas",
+    "price": 2000,
+    "img": "/assets/public/naruto.jpg"
   }, {
     "id": 6,
-    "title": "Cardify",
-    "category": "Pathfinder",
-    "price": 36
+    "title": "Naruto",
+    "detail": "Manga de 50 paginas",
+    "price": 2000,
+    "img": "/assets/public/naruto.jpg"
   }]
 
-/* function getItems(){
+function getItems(){
   return new Promise((resolve, reject)=>{
     setTimeout(()=>{
       resolve(items)
     },2000)
   })
 }
-export default getItems;  */
+export default getItems; 
 
-export function getSingleItem(){
-  return new Promise((resolve, reject)=>{
+export function getSingleItem(idItem){
+ /*  return new Promise((resolve, reject)=>{
     setTimeout(()=>{
       resolve(items[3])
     },2000)
+  }) */
+  return new Promise ((resolve, reject)=>{
+    let itemFind= items.find((item)=>{
+      console.log("find" , item.id, idItem);
+      
+      return item.id === parseInt(idItem)
+    })
+    if(itemFind) resolve(itemFind);
+    else reject (new Error("item no encontrado"))
   })
 }
 
--- src/components/itemDetailContainer/itemDetail.jsx
+++ src/components/itemDetailContainer/itemDetail.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./itemDetail.css"
-function itemDetail(props) {
-    let {id, title, img, detail, price} = props
+
+function itemDetail({items}) {
+    /*  let {id, title, img, detail, price} = props   */
+ 
+  
    
     return(
         <div>
             
-            <h2 className="">{title}</h2>
+            <h2 className="">{items.title}</h2>
         <div className="card">
             <div className="container">
-            <h3>{id}</h3>
-            <img src={img} className="imgDetail" alt={img} />
+            <h3>{items.id}</h3>
+            <img src={items.img} className="imgDetail" alt={items.img} />
             </div>
             </div>
             <div>
-            <p className="cardDetails">{detail}</p>
-            <p className="priceCard">${price}</p>
+            <p className="cardDetails">{items.detail}</p>
+            <p className="priceCard">${items.price}</p>
             </div>
            </div>
         
