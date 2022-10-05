f8338cf2a3429f2f1c0c23dde144064276f84db8
Vicenciomf <vxmartinez@ug.uchile.cl>
2022-10-05 15:56:58 -0300

    Debbugeo
    Hola! Vi tu mensaje en el foro y quise ayudar, lo que hice fue lo siguiente:
    -Buenas prácticas como indentaciones, los punto y comas, el agregar de nuevo el Strict Mode, el arreglar el useEffect para que se montara cada vez que cambiara el estado con el que hacías el fetch.
    -Arreglar el bug del detalle de tu producto, el problema consistía en el cómo manejabas las props.
    -Agregar la dependencia del react router a tu package.json, para tu siguiente entrega.
    
    Espero haber sido de ayuda, saludos!

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -1,38 +1,21 @@
-import ItemDetail from './itemDetail'
+import ItemDetail from './itemDetail';
 import React, {useState, useEffect} from 'react';
-import {getSingleItem} from '../../Services/mockAPI'
-import {useParams} from "react-router-dom"
+import {getSingleItem} from '../../Services/mockAPI';
+import {useParams} from "react-router-dom";
 
 function ItemDetailContainer() {
-let [items, setItems]= useState({});
+    let [items, setItems] = useState({});
+    const {id} = useParams();
 
-const { id }= useParams()    
-  
-useEffect(()=>{
-     getSingleItem(id).then((respuesta) => setItems(respuesta))
-  },[]);
-    return(
+    useEffect(() => {
+        getSingleItem(id).then((respuesta) => setItems(respuesta));
+    }, [id]); //Debes volver a montar el componente cada vez que cambie el id, o sólo te funcionará bien el fetch una vez
+
+    return (
         <div>
-           
-          
-          <ItemDetail items={items} /> 
-          
-            
-            
-            {/* <div className='card'>
-            <h1>{items.id}</h1>
-             <h2>{items.title}</h2>
-              <img src="{items.img}" alt="{items.img}" />
-               <p>{items.detail}</p>
-                <p>{items.price}</p>
-                </div> */}
-          </div> 
-    )
-    }   
-    
-   
-
-  
-//)
+            <ItemDetail items={items}/>
+        </div>
+    );
+}
 
 export default ItemDetailContainer;
